484e0e41e4187f67521d95b568eb23f6dd2aee5f
wanjiiru <liz240@live.com>
2018-06-14 12:43:11 +0300

    add news api key

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from .config import DevConfig
 
+#initiliazing application
+app = Flask(__name__, instance_relative_config=True)
 
-app = Flask(__name__)
+#setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 
 from app import views
